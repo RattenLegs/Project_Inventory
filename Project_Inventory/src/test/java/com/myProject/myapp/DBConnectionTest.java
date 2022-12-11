e9cf959bb2d816a47a2b4c1c64f63fcac172f630
Rattanlegs <Rattanlegs@gmail.com>
2022-12-11 23:10:28 +0900

    아이고빡세다

---
+++ Project_Inventory/src/test/java/com/myProject/myapp/DBConnectionTest.java
@@ -0,0 +1,39 @@
+package com.myProject.myapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class DBConnectionTest {
+
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String uid = "spring";
+	private String upw = "spring";
+
+	//DB 연결 테스트
+	@Test
+	public void connectTest() {
+		Connection conn = null;
+		
+		try {
+			Class.forName(driver);
+			
+			conn = DriverManager.getConnection(url, uid, upw);
+			System.out.println("DB 커넥션 성공!");
+			System.out.println("conn: " + conn);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
