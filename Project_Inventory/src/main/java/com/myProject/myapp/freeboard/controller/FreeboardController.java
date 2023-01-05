fb2292f9553b5bb40447d28f8ad417eee267bc0f
Rattanlegs <Rattanlegs@gmail.com>
2023-01-05 23:46:58 +0900

    후우..

--- Project_Inventory/src/main/java/com/myProject/myapp/freeboard/controller/FreeboardController.java
+++ Project_Inventory/src/main/java/com/myProject/myapp/freeboard/controller/FreeboardController.java
@@ -12,22 +12,8 @@ public class FreeboardController {
 
 	}
 
-	/*
-	 * @RequestMapping(value = "/freeboard/freeboard", method = RequestMethod.GET)
-	 */
-	@GetMapping("/freeboard")
-	/* 
-	 * public String repairList() { return "/freeboard/freeboard"; } 
-	 * Mapping과 freeboard 같으니 void로 처리
-	 * void면 viewResolver에게 줄 수 있는 문자열이 없으니 url을 전달
-	 * return에 쓸 문자열과 url이 같으면 void로 처리하면 됨
-	 * */
-	public void repairList() {}
-		/*
-		 * @RequestMapping(path = "/freeboard/freeboard/test", method =
-		 * RequestMethod.GET) public String testCall() {
-		 * System.out.println("test 요청 들어옴"); return "test"; //test는 dispatchersevlet으로,
-		 * d는 test를 viewResolver에게, viewResolver는 test 앞에 prefix, 뒤에 suffix 붙임 }
-		 */
+	@GetMapping("/freeList")
+	public void freeList() {}
+
 
 }
