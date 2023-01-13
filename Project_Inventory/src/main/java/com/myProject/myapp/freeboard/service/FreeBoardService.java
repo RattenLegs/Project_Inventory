a00cefb888b815d865e74aea130f4fc4d93f577f
Rattanlegs <Rattanlegs@gmail.com>
2023-01-13 23:16:12 +0900

    뭐라도해보자

--- Project_Inventory/src/main/java/com/myProject/myapp/freeboard/service/FreeBoardService.java
+++ Project_Inventory/src/main/java/com/myProject/myapp/freeboard/service/FreeBoardService.java
@@ -2,48 +2,58 @@ package com.myProject.myapp.freeboard.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.myProject.myapp.freeboard.mapper.IFreeBoardMapper;
 import com.myProject.myapp.freeboard.vo.FreeBoardVO;
 import com.myProject.myapp.util.vo.PageVO;
 
 @Service
 public class FreeBoardService implements IFreeBoardService {
 
+	@Autowired
+	private IFreeBoardMapper mapper;
+	
 	@Override
 	public void regist(FreeBoardVO vo) {
-		// TODO Auto-generated method stub
-
+		mapper.regist(vo);
 	}
 
 	@Override
 	public List<FreeBoardVO> getList(PageVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<FreeBoardVO> list = mapper.getList(vo);
+		
+		for(FreeBoardVO article : list) {
+			long now = System.currentTimeMillis();
+			long regTime = article.getRegDate().getTime();
+			
+			if(now - regTime < 60 * 60 * 24 * 2 * 1000) {
+				article.setNewMark(true);
+			}
+		}
+		
+		return list;
 	}
 
 	@Override
 	public int getTotal(PageVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		return mapper.getTotal(vo);
 	}
 
 	@Override
 	public FreeBoardVO getContent(int bno) {
-		// TODO Auto-generated method stub
-		return null;
+		return mapper.getContent(bno);
 	}
 
 	@Override
 	public void update(FreeBoardVO vo) {
-		// TODO Auto-generated method stub
-
+		mapper.update(vo);
 	}
 
 	@Override
 	public void delete(int bno) {
-		// TODO Auto-generated method stub
-
+		mapper.delete(bno);
 	}
-
 }
